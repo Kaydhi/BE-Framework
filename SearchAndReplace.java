cc1cd422aea74a386bf27fde45d51420f619f441
Careilmax <careilmaxime@gmail.com>
2015-12-04 15:47:01 +0100

    Update SearchAndReplace.java

--- SearchAndReplace.java
+++ SearchAndReplace.java
@@ -11,77 +11,93 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class SearchAndReplace {
+public class SearchAndReplace
+{
 
-    public static void main(String[] args) {
-        String dataFileIn = args[0];
-        String replaceFile = args[1];
-        String dataFileOut = args[2];
+	public static void main(String[] args)
+	{
+		String dataFileIn = args[0];	//Paramètres d'entrée
+		String replaceFile = args[1];
+		String dataFileOut = args[2];
 
- //       String dataFileIn = "PreTraitement.java";
- //       String replaceFile = "PreTraitement.xml";
- //     String dataFileOut = "PreTraitement_post.java";
+		// String dataFileIn = "PreTraitement.java";
+		// String replaceFile = "PreTraitement.xml";
+		// String dataFileOut = "PreTraitement_post.java";
 
-        StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder(); //Permet de constuire des strings
 
-        try {
-            InputStream ips = new FileInputStream(dataFileIn);
-            InputStreamReader ipsr = new InputStreamReader(ips);
-            BufferedReader br = new BufferedReader(ipsr);
-            String ligne;
-            while ((ligne = br.readLine()) != null) {
-                sb.append(ligne);
-                sb.append("\n");
-            }
-            br.close();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
+		try
+		{
+			InputStream ips = new FileInputStream(dataFileIn);	//Designe un fichier à lire
+			InputStreamReader ipsr = new InputStreamReader(ips);  //Permet de lire le fichier
+			BufferedReader br = new BufferedReader(ipsr);	//Créer un buffer
+			String ligne;
+			while ((ligne = br.readLine()) != null)	//Lecture intégrale du fichier
+			{
+				sb.append(ligne);	//Insert un string dans le string builder et le concatène
+				sb.append("\n");
+			}
+			br.close();	//Fermeture du fichier
+		}
+		catch (Exception e)	//Exception
+		{
+			System.out.println(e.toString());	//On affiche l'exception
+		}
 
-        String out = sb.toString();
-        HashMap<String, String> dict = new HashMap<>();
-        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		String out = sb.toString();	//On affiche le string généré par le string builder
+		HashMap<String, String> dict = new HashMap<>();	//Création d'un HashMap ("Dictionnary")
+		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		final DocumentBuilder builder;
+		final Document document;
+		final Element racine;
+		try
+		{
+			builder = factory.newDocumentBuilder();
+			document = builder.parse(new File(replaceFile));	
+			racine = document.getDocumentElement();	
 
-        final DocumentBuilder builder;
-        final Document document;
-        final Element racine;
-        try {
-            builder = factory.newDocumentBuilder();
-            document = builder.parse(new File(replaceFile));
-            racine = document.getDocumentElement();
+			final NodeList racineNoeuds = racine.getChildNodes();
+			final int nbRacineNoeuds = racineNoeuds.getLength();
 
-            final NodeList racineNoeuds = racine.getChildNodes();
-            final int nbRacineNoeuds = racineNoeuds.getLength();
+			for (int i = 0; i < nbRacineNoeuds; i++) //Récupère les valeur du XML, pour le mettre dans le dictionnary
+			{
+				if (racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE)
+				{
+					final Element value = (Element) racineNoeuds.item(i);
+					String key = "\\{" + value.getAttribute("name") + "\\}";
+					dict.put(key, value.getTextContent());
+				}
+			}
+		}
+		catch (ParserConfigurationException e)
+		{
+			e.printStackTrace();
+		}
+		catch (SAXException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 
-            for (int i = 0; i < nbRacineNoeuds; i++) {
-                if (racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+		for (Map.Entry<String, String> entry : dict.entrySet())	//On lit le dictionnary
+		{
+			out = out.replaceAll(entry.getKey(), entry.getValue());	//On met tout dans un string
+		}
 
-                    final Element value = (Element) racineNoeuds.item(i);
-                    String key = "\\{" + value.getAttribute("name") + "\\}";
-                    dict.put(key, value.getTextContent());
-
-                }
-            }
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for(Map.Entry<String, String> entry : dict.entrySet()) {
-            out = out.replaceAll(entry.getKey(), entry.getValue());
-        }
-
-        try {
-            FileWriter fw = new FileWriter(dataFileOut);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter fichierSortie = new PrintWriter(bw);
-            fichierSortie.println(out);
-            fichierSortie.close();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-    }
+		try
+		{
+			FileWriter fw = new FileWriter(dataFileOut);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter fichierSortie = new PrintWriter(bw);
+			fichierSortie.println(out);	//On écrit le string dans un fichier
+			fichierSortie.close();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+	}
 }
