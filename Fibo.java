777ed80acaad46eec099dcb5c585b9ab15cc9a3a
Kaydhi <kaydhi555@gmail.com>
2015-12-09 13:54:23 +0100

    Update Fibo.java

--- Fibo.java
+++ Fibo.java
@@ -9,30 +9,47 @@ public class Fibo {
 		if (!(entree.exists())){
 			try{
 			entree.createNewFile();		
-			}catch(IOException i){}
+			}catch(IOException i){i.printStackTrace();}
 		}
+		File sortie = new File("Sortie.txt");
+		try{
+			entree.createNewFile();		
+			}catch(IOException i){i.printStackTrace();}
 		 
 		 try{
 		 BufferedReader br = new BufferedReader(new FileReader(entree)) ;
-		 BufferedWriter bw = new BufferedWriter(new FileWriter(entree)) ;
+		 BufferedWriter bw = new BufferedWriter(new FileWriter(sortie)) ;
 		 String ligne="";
 		 String tmp="";
 		  
+		 if (entree.exists()){
+			 System.out.println("Fichier existant!");
+		 }
+		 
+		 System.out.println(br.read());
 		 while ((ligne = br.readLine()) != null){
 			 tmp=ligne;
-		     bw.write(ligne+"\n");
+		     System.out.println("lign:"+ligne);
+		     bw.write(ligne+" \n");
 			 bw.flush();
 		  }
+		 
 		 String sValeur[]=tmp.split(" ");
+		 System.out.println(tmp);
 		 if (sValeur[0].equals("")){
-			 bw.write("1 1");
+			 bw.write("0 1 \n");
+
 		 }
 		 else{
-			 bw.write(sValeur[1]+Integer.toString(Integer.parseInt(sValeur[0]+Integer.parseInt(sValeur[1]))));
+			 int val = Integer.parseInt(sValeur[0])+Integer.parseInt(sValeur[1]);
+			 bw.write(sValeur[1]+" "+Integer.toString(val)+"\n");
 			 bw.flush();
 		 }
+		 
 		 br.close();
 		 bw.close();
+		 entree.delete();
+		 sortie.renameTo(new File("Fibo.txt"));
 		 }catch (FileNotFoundException e) {
 		 }catch (IOException l){}
 		 
